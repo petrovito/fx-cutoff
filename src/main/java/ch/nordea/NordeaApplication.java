7542e35068e709f79c2938ad2fe9f085a8950d10
Pjotr Zarandy <zarandy.peter@gmail.com>
2023-03-19 13:19:47 +0100

    init

---
+++ src/main/java/ch/nordea/NordeaApplication.java
@@ -0,0 +1,13 @@
+package ch.nordea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NordeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/nordea/NordeaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.nordea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
